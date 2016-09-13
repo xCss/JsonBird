a793ac35ddae5361b8a7dd2a8e2b6e97be66f304
dotos <yy.liu@foxmail.com>
2016-09-14 00:49:21 +0800

    fix some error

--- configs/config.js
+++ configs/config.js
@@ -2,8 +2,7 @@ module.exports = {
     //site config
     site: {
         title: 'JsonBird',
-        author: 'xCss',
-        concat: 'xioveliu@gmail.com',
+        author: 'xCss <xioveliu@gmail.com>',
         description: '业界领先的远程 JSON 代理服务'
     },
     //mysql config
